3ba21d627e2a61ae609230c6a339b3b7982f5946
Jose Flores <jose.flores@pccomponentes.com>
2020-02-26 09:05:07 +0100

    fix file indentation

--- src/DomainTrace/Guzzle/DomainTraceMiddleware.php
+++ src/DomainTrace/Guzzle/DomainTraceMiddleware.php
@@ -10,17 +10,17 @@ final class DomainTraceMiddleware
     public static function trace(Tracker $tracker): \Closure
     {
         return static fn (callable $handler) => static function ($request, array $options) use ($handler, $tracker) {
-                $request = $request->withHeader(
-                    'x-correlation-id',
-                    $tracker->correlationId(),
-                );
+            $request = $request->withHeader(
+                'x-correlation-id',
+                $tracker->correlationId(),
+            );
 
-                $request = $request->withHeader(
-                    'x-reply-to',
-                    $tracker->replyTo(),
-                );
+            $request = $request->withHeader(
+                'x-reply-to',
+                $tracker->replyTo(),
+            );
 
-                return $handler($request, $options);
+            return $handler($request, $options);
         };
     }
 }
