d5826faae97c110cc696f3d3f6cfd09100c684dc
Fran Moraton <franciscoa.moraton@pccomponentes.com>
2021-11-11 10:22:28 +0100

    add use for RedeliveryStamp

--- src/MessageLogger/MessageLoggerMiddleware.php
+++ src/MessageLogger/MessageLoggerMiddleware.php
@@ -8,6 +8,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\RedeliveryStamp;
 
 final class MessageLoggerMiddleware implements MiddlewareInterface
 {
